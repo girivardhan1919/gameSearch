9259b59ead7df1ac0ba479229d3bd9327a1a5050
Giri Vardhan <56465249+girivardhan1919@users.noreply.github.com>
2022-07-19 20:32:34 +0530

    Add files via upload

---
+++ src/data/initialDetails.js
@@ -0,0 +1,40 @@
+const initialDetails = [
+  {
+    id: 1,
+    imgPath: "/assets/img/1.png",
+    name: "LittleBigPlanet PS Vita",
+    playStation: "PlayStation Vita",
+    genre: "Platformer",
+  },
+  {
+    id: 2,
+    imgPath: "/assets/img/2.png",
+    name: "Splice: Tree of Life",
+    playStation: "iPad",
+    genre: "Puzzle",
+  },
+  {
+    id: 3,
+    imgPath: "/assets/img/3.png",
+    name: "Total War Battles: Shogun",
+    playStation: "Macintosh",
+    genre: "Strategy",
+  },
+  {
+    id: 4,
+    imgPath: "/assets/img/4.png",
+    name: "Double Dragon: Neon",
+    playStation: "Xbox 360",
+    genre: "Fighting",
+  },
+  {
+    id: 5,
+    imgPath: "/assets/img/5.png",
+    name: "Mark of the Ninja",
+    playStation: "Xbox 360",
+    genre: "Action, Adventure",
+  },
+  
+];
+
+export default initialDetails;
